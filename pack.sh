8b6b036ce327ed83f4ebf15a2ed4c9981993d3d7
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2025-03-27 22:35:04 -0500

    Change file name to pack

---
+++ pack.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#Variables
+DOMAIN_NAME=PostventaEAI_001
+JAR_FILE_NAME=PostventaEAI_001.jar
+COMMON_DIR="/u01/oracle/product/wls12214/oracle_common/common/bin"
+DOMAIN_DIR="/u01/oracle/domains"
+NODE_USER=
+NODE_IP=
+
+
+error_acceso_directorio(){
+    echo "Error: ($?) $1"
+    exit 1
+}
+
+#=====ADMIN=====
+#TODO: AGREGAR PARA N NODOS 
+
+echo '===== Iniciando el enpaquetado..... ====='
+
+cd "$COMMON_DIR" || error_acceso_directorio
+
+/pack.sh -domain="$DOMAIN_DIR/$DOMAIN_NAME" -template="$DOMAIN_DIR/$JAR_FILE_NAME" -managed=true -template_name="$DOMAIN_NAME"
+
+echo '==== Copiando el pack generado ====='
+
+cd "$DOMAIN_DIR" || error_acceso_directorio
+cp "$JAR_FILE_NAME" /home/weblogic
+
+echo '==== Transfierdon el pack al nodo ====='
+
+#validamos si el archivo se encutra
+if [ ! -f "$JAR_FILE_NAME" ]; then
+    echo "El archivo no existe"
+    exit 1
+else
+    echo "El archivo se encuentra en el directorio"
+fi
+
+scp "/home/weblogic/$JAR_FILE_NAME" "$NODE_USER@$NODE_IP:/home/weblogic"
+
+
+echo '==== Iniciando el unpack..... ===='
+
+if ! ssh ${NODE_USER}@${NODE_IP} "[ -f /home/weblogic/${JAR_FILE_NAME} ]"; then
+    echo "ERROR: El archivo ${JAR_FILE_NAME} no existe en el nodo"
+    exit 1
+fi
+
+if ! ssh ${NODE_USER}@${NODE_IP} "bash -s" < unpack.sh; then
+    echo "ERROR: Falló la ejecución remota del unpack"
+    exit 1
+fi
