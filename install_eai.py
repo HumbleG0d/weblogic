dde6af367d60c5a0ff1594a75357bc9147dc8d4e
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2025-03-31 20:59:37 -0500

    🔨 [ADD] install domain OSB

--- install_eai.py
+++ install_eai.py
@@ -9,39 +9,32 @@ def get_script_path():
 def parsefile():
     propfile = get_script_path() + "/domain.properties"
     if exists(propfile):
-        global fo
-        fo = open(propfile, 'r+')
-        lines = fo.readlines()
-        for line in lines:
-            if "=" in line:
-                line = line.rstrip()
-                key = line.split('=')[0]
-                value = line.split('=')[1]
-                _dict[key] = value
+        global _dict
+        _dict = {}
+        with open(propfile, 'r') as fo:
+            lines = fo.readlines()
+            for line in lines:
+                if "=" in line and not line.strip().startswith('#'):
+                    line = line.rstrip()
+                    key = line.split('=')[0].strip()
+                    value = line.split('=')[1].strip()
+                    _dict[key] = value
 
 def printdomain():
-    print('------------------------------')
+    print('\n------------------------------')
     print("Properties Information")
     print('------------------------------')
     for key, val in _dict.items():
-        print(key, "=>", val)
+        if 'password' in key.lower():
+            print(f"{key} => {'*' * len(val)}")
+        else:
+            print(f"{key} => {val}")
 
 def export_properties():
-    global _dict
-    global mwhome
-    global wlshome
-    global domainroot
-    global approot
-    global domainName
-    global domain_username
-    global domain_password
-    global domain_confirm_password
-    global adminPort
-    global adminAddress
-    global machines
-    global servers
-    global clusters
-
+    global mwhome, wlshome, domainroot, approot
+    global domainName, domain_username, domain_password, domain_confirm_password
+    global adminPort, adminAddress, machines, servers, clusters
+    
     mwhome = _dict.get('mwhome')
     wlshome = _dict.get('wlshome')
     domainroot = _dict.get('domainroot')
@@ -50,19 +43,74 @@ def export_properties():
     domain_username = _dict.get('domain_username')
     domain_password = _dict.get('domain_password')
     domain_confirm_password = _dict.get('domain_confirm_password')
+    adminPort = _dict.get('admin.port')
+    adminAddress = _dict.get('admin.address')
+    machines = _dict.get('machines', '').split(',')
+    servers = _dict.get('managedservers', '').split(',')
+    clusters = _dict.get('clusters', '').split(',')
 
-    adminPort = _dict.get("admin.port")
-    adminAddress = _dict.get("admin.address")
+def validate_domain_properties():
+    required_keys = [
+        'mwhome', 'wlshome', 'domainroot',
+        'domain_name', 'domain_username', 'domain_password',
+        'admin.port', 'admin.address'
+    ]
+    
+    missing_keys = [key for key in required_keys if key not in _dict]
+    if missing_keys:
+        print(f"ERROR: Faltan propiedades obligatorias: {', '.join(missing_keys)}")
+        sys.exit(1)
+        
+    if _dict.get('domain_password') != _dict.get('domain_confirm_password'):
+        print("ERROR: La contraseña y su confirmación no coinciden")
+        sys.exit(1)
 
-    machines = _dict.get("machines").split(',')
-    servers = _dict.get("managedservers").split(',')
-    clusters = _dict.get("clusters").split(',')
+def configure_datasources():
+    try:
+        print("\nConfigurando DataSources para componentes OSB...")
+        
+        # Configuración común para todos los DataSources
+        db_url = _dict.get('database.url')
+        db_driver = 'oracle.jdbc.OracleDriver'
+        test_query = 'SQL SELECT 1 FROM DUAL'
+        
+        # Lista de componentes que necesitan DataSources
+        components = ['SOAINFRA', 'ORASDPM', 'ORASDPS', 'MDS', 'OPSS']
+        
+        for component in components:
+            schema_prefix = _dict.get('rcu.schema_prefix', 'OSB')
+            schema_user = f"{schema_prefix}_{component}"
+            schema_password = _dict.get('database.schema.password')
+            
+            print(f"\nCreando DataSource para {component}...")
+            
+            cd('/')
+            create(f'{component}_DataSource', 'JDBCSystemResource')
+            cd(f'/JDBCSystemResources/{component}_DataSource/JDBCResource/{component}_DataSource')
+            create('myJdbcParams', 'JDBCConnectionPoolParams')
+            set('DriverName', db_driver)
+            set('URL', db_url)
+            set('User', schema_user)
+            set('Password', schema_password)
+            set('TestTableName', test_query)
+            
+            print(f"DataSource {component}_DataSource configurado")
+            
+    except Exception as e:
+        print(f"ERROR al configurar DataSources: {str(e)}")
+        print("Dumpstack: \n -------------- \n", dumpStack())
+        sys.exit(1)
 
 def read_template():
     try:
-        readTemplate(wlshome + '/common/templates/wls/wls.jar')
+        template_path = _dict.get('template')
+        if not template_path:
+            template_path = f"{mwhome}/osb/common/templates/wls/osb.jar"
+            
+        print(f"\nLeyendo template desde: {template_path}")
+        readTemplate(template_path)
     except:
-        print("Error Reading the Template", wlshome)
+        print("Error leyendo el template")
         print("Dumpstack: \n -------------- \n", dumpStack())
         sys.exit(2)
 
@@ -70,248 +118,142 @@ def create_machine():
     try:
         cd('/')
         for machine in machines:
-            print("Creating a New Machine with the following Configuration")
-            mn = create(machine, 'Machine')
-            machine_name = _dict.get(machine + '.Name')
+            if not machine: continue
+            
+            print("\nCreando nueva máquina...")
+            machine_name = _dict.get(f"{machine}.Name")
             if machine_name:
-                print("\tMachine Name:", machine_name)
-                mn.setName(machine_name)
+                create(machine_name, 'Machine')
+                print(f"Máquina creada: {machine_name}")
             else:
-                print("No machine Name mentioned for", machine)
+                print(f"No se especificó nombre para la máquina {machine}")
     except:
-        print("Creating Machine failed", machine)
+        print("Error creando máquina")
         print("Dumpstack: \n -------------- \n", dumpStack())
         sys.exit(2)
 
 def create_admin():
     try:
-        print("\nCreating AdminServer with the following Configuration")
+        print("\nConfigurando AdminServer...")
         
-        # Validar que la contraseña y su confirmación coincidan
-        if _dict.get('domain_password') != _dict.get('domain_confirm_password'):
-            print("ERROR: La contraseña y su confirmación no coinciden.")
-            sys.exit(1)
-
-        # Configurar el usuario administrador del dominio
         cd('/Security/base_domain/User/' + domain_username)
-        cmo.setPassword(domain_password)  # Configura la contraseña del usuario administrador
+        cmo.setPassword(domain_password)
+        
         cd('/Servers/AdminServer')
         cmo.setName('AdminServer')
         cmo.setListenPort(int(adminPort))
         cmo.setListenAddress(adminAddress)
-        print("\tAdminServer ListenPort:", adminPort)
-        print("\tAdminServer ListenAddress:", adminAddress)
-
+        
+        print(f"AdminServer configurado en {adminAddress}:{adminPort}")
     except:
-        print("Error while creating AdminServer")
+        print("Error configurando AdminServer")
         print("Dumpstack: \n -------------- \n", dumpStack())
+        sys.exit(1)
 
 def create_managedserver():
     try:
         cd('/')
         for server in servers:
-            MSN = _dict.get(server + '.Name')  # Nombre del servidor gestionado
-            MSP = _dict.get(server + '.port')  # Puerto del servidor gestionado
-            MSA = _dict.get(server + '.address')  # Dirección IP del servidor gestionado
-            MSUser = _dict.get(server + '.username')  # Usuario del Node Manager
-            MSPass = _dict.get(server + '.password')  # Contraseña del Node Manager
-            MSConfirmPass = _dict.get(server + '.confirm_password')  # Confirmación de la contraseña
-
-            print("\nCreating A New Managed Server with following Configuration")
-            print("\tServerName:", MSN)
-            print("\tServer ListenPort:", MSP)
-            print("\tServer ListenAddress:", MSA)
-            print("\tNode Manager Username:", MSUser)
-            print("\tNode Manager Password: ********")  # No imprimir la contraseña en texto plano
-
-            # Crear el servidor gestionado
-            sobj = create(MSN, 'Server')
-            sobj.setName(MSN)
-            sobj.setListenPort(int(MSP))
-            sobj.setListenAddress(MSA)
-
-            # Asignar el servidor gestionado a una máquina (Machine)
-            cd('/Servers/' + MSN)
-            cmo.setMachine(getMBean('/Machines/' + _dict.get('m2.Name')))  # Asigna a la máquina correcta
-
+            if not server: continue
+            
+            server_name = _dict.get(f"{server}.Name")
+            server_port = _dict.get(f"{server}.port")
+            server_address = _dict.get(f"{server}.address")
+            
+            if server_name and server_port and server_address:
+                print(f"\nCreando Managed Server: {server_name}")
+                create(server_name, 'Server')
+                cd(f'/Servers/{server_name}')
+                cmo.setListenPort(int(server_port))
+                cmo.setListenAddress(server_address)
+                print(f"Managed Server {server_name} creado en {server_address}:{server_port}")
     except:
-        print("Error While Creating ManagedServer", server)
+        print("Error creando Managed Server")
         print("Dumpstack: \n -------------- \n", dumpStack())
+        sys.exit(1)
 
 def create_clusters():
     try:
         cd('/')
         for cluster in clusters:
-            CN = _dict.get(cluster + '.Name')
-            cobj = create(CN, 'Cluster')
-            print("\nCreating a New Cluster with the following Configuration")
-            print("\tClusterName:", CN)
+            if not cluster: continue
+            
+            cluster_name = _dict.get(f"{cluster}.Name")
+            if cluster_name:
+                print(f"\nCreando cluster: {cluster_name}")
+                create(cluster_name, 'Cluster')
+                print(f"Cluster {cluster_name} creado")
     except:
-        print("Error while Creating Cluster", cluster)
+        print("Error creando cluster")
         print("Dumpstack: \n -------------- \n", dumpStack())
         sys.exit(2)
 
 def commit_writedomain():
     try:
+        print("\nGuardando configuración del dominio...")
         setOption('OverwriteDomain', 'true')
         setOption('ServerStartMode', 'prod')
         setOption('AppDir', approot + '/' + domainName)
         writeDomain(domainroot + '/' + domainName)
         closeTemplate()
+        print(f"Dominio {domainName} creado en {domainroot}")
     except:
-        print("ERROR: commit_writedomain Failed")
+        print("Error guardando dominio")
         print("Dumpstack: \n -------------- \n", dumpStack())
         undo('false', 'y')
         stopEdit()
-        exit()
-
-def print_withformat(title):
-    print("\n-----------------------------------------------------\n", title, "\n-----------------------------------------------------")
-
-def print_somelines():
-    print("-----------------------------------------------------")
-
-def print_domainsummary():
-    print("DomainName:", domainName)
-    print("DomainUserName:", domain_username)
-    print("DomainPassword: ****************")
-    print("DomainDirectory:", domainroot)
-    print("ApplicationRoot:", approot)
+        sys.exit(1)
 
 def start_AdminServer():
     try:
-        global managementurl
-        managementurl = "t3://" + adminAddress + ":" + adminPort
-        global AdminServerDir
-        AdminServerDir = domainroot + "/" + domainName + "/servers/AdminServer"
-        global AdminServerLogDir
-        AdminServerLog = AdminServerDir + "/logs/AdminServer.log"
-        global DomainDir
-        DomainDir = domainroot + "/" + domainName
-
-        print_somelines()
-        print("\nStarting Server with following Params")
-        print_somelines()
-        print("DomainDir", DomainDir)
-        print("managementurl", managementurl)
-        print_somelines()
-
-        print("\nRedirecting Startup Logs to", AdminServerLog)
-        startServer('AdminServer', domainName, managementurl, domain_username, domain_password, DomainDir, 'true', 60000, serverLog=AdminServerLog)
-
-        print("AdminServer has been successfully Started")
+        print("\nIniciando AdminServer...")
+        domain_path = domainroot + '/' + domainName
+        managementurl = f"t3://{adminAddress}:{adminPort}"
+        log_file = f"{domain_path}/servers/AdminServer/logs/AdminServer.log"
+        
+        print(f"URL de gestión: {managementurl}")
+        print(f"Log file: {log_file}")
+        
+        startServer('AdminServer', domainName, managementurl, 
+                   domain_username, domain_password, 
+                   domain_path, 'true', 60000, serverLog=log_file)
+        
+        print("AdminServer iniciado correctamente")
     except:
-        print("ERROR: Unable to Start AdminServer")
+        print("Error iniciando AdminServer")
         print("Dumpstack: \n -------------- \n", dumpStack())
-
-def connect_online():
-    try:
-        global managementurl
-        managementurl = "t3://" + adminAddress + ":" + adminPort
-        print("\nConnecting to AdminServer with managementurl", managementurl)
-        connect(domain_username, domain_password, managementurl)
-        print("\nSuccessfully Connected to AdminServer!!.")
-    except:
-        print("ERROR: Unable to Connect to AdminServer")
-        sys.exit(2)
-
-def acquire_edit_session():
-    edit()
-    startEdit()
-
-def save_activate_session():
-    save()
-    activate()
-
-def Enable_wlst_log_redirection():
-    redirect('./wlst_execution.log', 'false')
-
-def Stop_wlst_log_redirection():
-    stopRedirect()
-
-def map_machines():
-    acquire_edit_session()
-    for machine in machines:
-        print("Starting to map resources to the machine", machine)
-        instances = _dict.get(machine + ".instances")
-        if len(instances) > 1:
-            instances = instances.split(',')
-            for instance in instances:
-                if instance == "admin":
-                    instname = "AdminServer"
-                else:
-                    instname = _dict.get(instance + ".Name")
-                cd('/Servers/' + instname)
-                machine_name = _dict.get(machine + '.Name')
-                mbean_name = '/Machines/' + machine_name
-                cmo.setMachine(getMBean(mbean_name))
-        else:
-            instname = _dict.get(instances + ".Name")
-            cd('/Servers/' + instname)
-            machine_name = _dict.get(machine + '.Name')
-            mbean_name = '/Machines/' + machine_name
-            cmo.setMachine(getMBean(mbean_name))
-    save_activate_session()
-
-def map_clusters():
-    acquire_edit_session()
-    for cluster in clusters:
-        print("\nStarting to map resources to the cluster", cluster)
-        members = _dict.get(cluster + ".members")
-        if len(members) > 1:
-            members = members.split(',')
-            for member in members:
-                if member == "admin":
-                    membername = "AdminServer"
-                else:
-                    membername = _dict.get(member + ".Name")
-                cd('/Servers/' + membername)
-                cluster_name = _dict.get(cluster + '.Name')
-                mbean_name = '/Clusters/' + cluster_name
-                cmo.setCluster(getMBean(mbean_name))
-        else:
-            membername = _dict.get(members + ".Name")
-            cd('/Servers/' + membername)
-            cluster_name = _dict.get(cluster + '.Name')
-            mbean_name = '/Clusters/' + cluster_name
-            cmo.setCluster(getMBean(mbean_name))
-    save_activate_session()
+        sys.exit(1)
 
 if __name__ != "__main__":
-    _dict = {}
-    Enable_wlst_log_redirection()
-    print("Start of the script Execution >>")
-    print("Parsing the properties file...")
+    print("\n=== Inicio de creación de dominio OSB ===")
+    
+    # Paso 1: Leer y validar propiedades
     parsefile()
-    print("Exporting the Properties to variables..")
     export_properties()
-    print("Creating Domain from Domain Template...")
+    validate_domain_properties()
+    printdomain()
+    
+    # Paso 2: Crear dominio desde template
     read_template()
-    print_withformat("Creating Machines")
+    
+    # Paso 3: Configurar componentes
     create_machine()
-    print_somelines()
-    print_withformat("Creating AdminServer")
     create_admin()
-    print_somelines()
-    print_withformat("Creating ManagedServers")
     create_managedserver()
-    print_somelines()
-    print_withformat("Creating Clusters")
     create_clusters()
-    print_somelines()
-    print("\nCommit and Saving the Domain")
+    
+    # Paso 4: Configurar DataSources para OSB
+    configure_datasources()
+    
+    # Paso 5: Guardar dominio
     commit_writedomain()
-    print_withformat("Domain Summary")
-    print_domainsummary()
-    print_somelines()
-    print("Starting the AdminServer")
+    
+    # Paso 6: Iniciar AdminServer
     start_AdminServer()
-    connect_online()
-    map_machines()
-    map_clusters()
-    print("End of Script Execution << \nGood Bye!")
-    Stop_wlst_log_redirection()
+    
+    print("\n=== Dominio OSB creado exitosamente ===")
     sys.exit(0)
 
 if __name__ == "__main__":
-    print("This script has to be executed with weblogic WLST")
+    print("Este script debe ejecutarse con WLST de WebLogic")
+    print("Uso: wlst.sh create_osb_domain.py")
